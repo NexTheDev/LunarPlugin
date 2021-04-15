38a204913e1c96744029e5ca558ab303df5250a2
Nex <76622788+NexTheDev@users.noreply.github.com>
2021-04-15 23:35:35 +0300

    Add files via upload

---
+++ LunarPlugin/LunarPlugin/Plugin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+// Do not change the namespace
+namespace LunarPlugin
+{
+    // Do not change the class name
+	public class Plugin
+    {        
+        // Plugin will not work with parameters e.g Execute(object obj1)
+        public static void Execute()
+        {
+            // Your code for the plugin e.g Class1.Method1();
+        }
+    }
+}
